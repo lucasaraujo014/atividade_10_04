97fb19638f3e71ece230af456667a10823bf58ba
lucasaraujo014 <lucas11_araujo@hotmail.com>
2024-04-15 16:39:47 -0300

    atualização da atividade

---
+++ arquivo.js
@@ -0,0 +1,36 @@
+// Arrays são como caixas dentro de outras caixas, que podem ser acessados atravez do seu indece entre cochetes []. O primeiro indece sempre começa com 0, portanto no array
+
+//const arr = [`Girafa, Camaleão, Zebra`, 3];
+
+// arr.push("qualquer coisa")
+// arr.push("Girrafa")
+//console.log(arr);
+
+//const pushArray = ['Leão', 'Tartaruga', 'Papagaio']
+//console.log(pushArray);
+//pushArray[1] = 'Borboleta'consol
+//console.log(pushArray);
+//const popArray = ['a', 'b', 'c', 'd' ]
+//console.log(popArray);
+//popArray.pop()
+//popArray.shift()
+//console.log(popArray);
+//const spliceArray = ["Janeiro", 'Fevereiro', 'Março', 'Abril']
+//console.log(spliceArray);
+
+//spliceArray.splice(0,0, "meses")
+//console.log(spliceArray);
+
+//const filterArray = [1, 80, 2, 20, 30, 100, 93729]
+//console.log(filterArray);
+//const novoarray = filterArray.filter((qualquerCoisa) =>{
+ //   return qualquerCoisa >10
+//})console.log(novoarray);
+//const arrayMap = [1,2,3,4,5]
+//const arrayModificado = arrayMap.map((x) => x*2);
+//console.log(arrayModificado);
+
+const foreachArray = ['Carlos','Andre','Julia', 'Akira' ]
+foreachArray.forEach((nome) => {
+    console.log(nome);
+})
